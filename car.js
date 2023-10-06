6149b8dfa02c4a8c3074050087d5cb13b910e6d5
nikhilpounikar <nikhilpounikar@gmail.com>
2023-10-06 14:46:41 +0530

    car movement achieved

--- car.js
+++ car.js
@@ -5,8 +5,20 @@ class Car{
         this.y=y;
         this.width=width;
         this.height=height;
+        this.controls = new Controls();
     }
 
+    update(){
+
+        if(this.controls.forward){
+            this.y -=2;
+        }
+
+        if(this.controls.reverse){
+            this.y +=2;
+        }
+
+    }
     draw(ctx){
 
         ctx.beginPath();
@@ -19,4 +31,6 @@ class Car{
 
         ctx.fill();
     }
+
+
 }
