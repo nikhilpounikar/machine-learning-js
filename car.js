eec0ab32185f2b22b4bf8fff40ce4c20b954b53b
nikhilpounikar <nikhilpounikar@gmail.com>
2023-10-06 15:50:35 +0530

    small changes

--- car.js
+++ car.js
@@ -16,7 +16,10 @@ class Car{
     }
 
     update(){
+        this.#move();
+    }
 
+    #move(){
         if(this.controls.forward){
             this.speed += this.acceleration;
         }
@@ -54,13 +57,8 @@ class Car{
             }
         }
 
-
-
         this.x -=Math.sin(this.angle)*this.speed;
         this.y -=Math.cos(this.angle)*this.speed;
-       // this.y -= this.speed;
-       
-
     }
 
     draw(ctx){
